a893cb152c557d03c71d1c0f7f1dc26df6ab64c6
maryam <maryam.taeid@edu.fh-joanneum.at>
2025-04-09 18:11:28 +0200

    Add Log4j logging to Main.java with INFO and ERROR messages –ex5_1 and  ex5_2 completed

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,4 +1,4 @@
-package at.fhj.msd;
+/*package at.fhj.msd;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -37,5 +37,27 @@ public class Main {
             logger.error("Fehler bei Division durch 0: " + e.getMessage());
             System.out.println("Error: " + e.getMessage());
         }
+    }
+}*/
+package at.fhj.msd;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    
+    public static void main(String[] args) {
+      
+        logger.info("It is an Info Message from log4j");
+        logger.error("It is an Error Message from log4j");
+        
+        
+        Calculator calculator = new Calculator();
+        logger.info("Addition: {}", calculator.add(10, 5));
+        logger.info("Subtraction: {}", calculator.minus(10, 5));
+        logger.info("Division: {}", calculator.divide(10, 5));
+        logger.info("Multiplication: {}", calculator.multiply(10, 5));
     }
 }
